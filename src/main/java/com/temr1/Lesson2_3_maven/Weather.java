3c0d729dbc0e329adf8005ed1da2a389ca5e1f4a
Danylo <danylo.shpak.2009@gmail.com>
2024-02-07 22:39:32 +0200

    update project "Weather"

--- src/main/java/com/temr1/Lesson2_3_maven/Weather.java
+++ src/main/java/com/temr1/Lesson2_3_maven/Weather.java
@@ -2,21 +2,22 @@ package com.temr1.Lesson2_3_maven;
 
 public class Weather {
     private final String time;
-    private final double averageTemp;
+    private final int averageTemp;
     public Weather(String time, String temp1, String temp2){
-        this.time = time;
+        char firstChar = Character.toUpperCase(time.charAt(0));
+        this.time = firstChar + time.substring(1);
 
-        String numberOfTemp1 = temp1.substring(0,temp1.length() - 1);
-        String numberOfTemp2 = temp2.substring(0,temp2.length() - 1);
+        String numberOfTemp1 = temp1.substring(1,temp1.length() - 1);
+        String numberOfTemp2 = temp2.substring(1,temp2.length() - 1);
 
-        averageTemp =  (double) (Integer.parseInt(numberOfTemp1) + Integer.parseInt(numberOfTemp2)) / 2;
+        averageTemp = (Integer.parseInt(numberOfTemp1) + Integer.parseInt(numberOfTemp2)) / 2;
     }
 
     public String getTime() {
         return time;
     }
 
-    public double getAverageTemp() {
+    public int getAverageTemp() {
         return averageTemp;
     }
 }
